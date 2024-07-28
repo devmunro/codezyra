fefeb2790610a89c47b98754baf57f2daaeb905a
devmunro <defmunro@gmail.com>
2024-07-28 15:14:21 +0100

    Fixes: Create Authentication Section with Tabs for Login and Signup #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,48 @@
+"use client";
 import Signup from "./signup/page";
 import Login from "./login/page";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [showTab, setShowTab] = useState("signup");
+
+  const handleTab = (tab: string) => {
+    setShowTab(tab);
+  };
+
   return (
-    <main className="bg-rose-900">
-    <div className="flex justify-center items-center">
-      <Signup/>
-      <Login/>
-    </div>
+    <main className="bg-rose-900 h-screen">
+      <div className="flex flex-col items-center bg-gray-900 md:p-8 h-full">
+        <div>
+          <div className="flex justify-end ">
+          <button
+              className={`${showTab === "signup" ? "bg-black" : "bg-gray-900"} text-white p-2`}
+              onClick={() => handleTab("signup")}
+            >
+              Signup
+            </button>
+            <button
+              className={`${showTab === "login" ? "bg-black" : "bg-gray-900"} text-white p-2`}
+              onClick={() => handleTab("login")}
+            >
+              Login
+            </button>
+          </div>
+
+          <div className="relative w-full h-full ">
+            {showTab === "signup" && (
+              <div className="animate-fadeIn">
+                <Signup />
+              </div>
+            )}
+            {showTab === "login" && (
+              <div className="animate-fadeIn">
+                <Login />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        "fade-in": {
+          "0%": {
+              opacity: 0
+          },
+          "100%": {
+              opacity: 1
+          },
+      },
+      
+      },
+      animation: {
+        fadeIn: 'fade-in 1s ',
+        
       },
     },
   },
